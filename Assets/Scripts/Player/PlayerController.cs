cb45caf65310a3f60c1e5742f26f0159a1046f85
Federico <fedealvareztorres@gmail.com>
2026-07-20 21:59:37 -0300

    Laser + Some animations

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -2,9 +2,16 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    [Header("Properties")]
     [SerializeField] private float speed = 8.0f;
     [SerializeField] private float jumpForce = 2.0f;
     [SerializeField] private LayerMask groundMask;
+
+    [Header("Animation")]
+    [SerializeField] private Sprite idleGunUp;
+    [SerializeField] private Sprite idleGunDown;
+
+    private Sprite _idleOriginal;
 
     private Rigidbody2D _rb;
     private SpriteRenderer _renderer;
@@ -17,6 +24,9 @@ public class PlayerController : MonoBehaviour
     private bool _isGrounded = false;
     private bool _isJumping = false;
     private bool _isFalling = true;
+    private bool _isRunning = false;
+    private bool _isDead = false;
+
     #endregion
 
     #region Unity Methods
@@ -26,59 +36,99 @@ public class PlayerController : MonoBehaviour
         _rb = GetComponent<Rigidbody2D>();
         _renderer = GetComponentInChildren<SpriteRenderer>();
         _animator = GetComponentInChildren<Animator>();
+
+        _idleOriginal = _renderer.sprite;
     }
 
     private void Update()
     {
-        _horizontalMovement = Input.GetAxisRaw("Horizontal");
-
-        if (Mathf.Approximately(_horizontalMovement, 0))
+        if (!_isDead)
         {
-            _animator.SetBool("Run", false);
+            _horizontalMovement = Input.GetAxisRaw("Horizontal");
+
+            if (Mathf.Approximately(_horizontalMovement, 0))
+            {
+                _animator.SetBool("Run", false);
+                _animator.SetInteger("YDirection", 0);
+                _isRunning = false;
+            }
+
+            if (Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.LeftArrow))
+            {
+                _renderer.flipX = true;
+                _animator.SetBool("Run", true);
+                _isRunning = true;
+            }
+            else if (Input.GetKeyDown(KeyCode.D) || Input.GetKeyDown(KeyCode.RightArrow))
+            {
+                _renderer.flipX = false;
+                _animator.SetBool("Run", true);
+                _isRunning = true;
+            }
+
+            if (Input.GetKeyDown(KeyCode.Space) && _isGrounded)
+            {
+                _canJump = true;
+                _animator.SetTrigger("Jump");
+                //_isJumping = true;
+            }
+
+            if (Input.GetKeyDown(KeyCode.W))
+            {
+                _animator.SetInteger("YDirection", 1);
+
+                if (_isRunning == false)
+                {
+                    print("Key Up");
+                    _renderer.sprite = idleGunUp;
+                }
+            }
+            else if (Input.GetKeyDown(KeyCode.S))
+            {
+                _animator.SetInteger("YDirection", -1);
+
+                if (_isRunning == false)
+                {
+                    print("Key Down");
+                    _renderer.sprite = idleGunDown;
+                }
+            }
+
+            if (Input.GetKeyUp(KeyCode.W) || Input.GetKeyUp(KeyCode.S))
+            {
+                _animator.SetInteger("YDirection", 0);
+
+                if (_isRunning == false)
+                {
+                    _renderer.sprite = _idleOriginal;
+                }
+            }
+
+            /*if (Input.GetKeyDown(KeyCode.W) && _isGrounded && !_isJumping)
+            {
+                _canJump = true;
+                _animator.SetTrigger("Jump");
+                _isJumping = true;
+            }*/
+
+            var origin = transform.position - new Vector3(0, -0.1f, 0);
+            _isGrounded = Physics2D.Raycast(origin, Vector3.down, 0.2f, groundMask);
+
+            /*
+            if (!_isJumping && _isGrounded)
+            {
+                _isJumping = false;
+                _isFalling = false;
+                _rb.gravityScale = 1;
+            }
+
+            if (_isJumping && _rb.linearVelocityY < 0)
+            {
+                _isJumping = false;
+                _isFalling = true;
+                _rb.gravityScale = 2;
+            }*/
         }
-
-        if (Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.LeftArrow))
-        {
-            _renderer.flipX = true;
-            _animator.SetBool("Run", true);
-        }
-        else if (Input.GetKeyDown(KeyCode.D) || Input.GetKeyDown(KeyCode.RightArrow))
-        {
-            _renderer.flipX = false;
-            _animator.SetBool("Run", true);
-        }
-
-        if (Input.GetKeyDown(KeyCode.W) && _isGrounded)
-        {
-            _canJump = true;
-            _animator.SetTrigger("Jump");
-            //_isJumping = true;
-        }
-
-        /*if (Input.GetKeyDown(KeyCode.W) && _isGrounded && !_isJumping)
-        {
-            _canJump = true;
-            _animator.SetTrigger("Jump");
-            _isJumping = true;
-        }*/
-
-        var origin = transform.position - new Vector3(0, -0.1f, 0);
-        _isGrounded = Physics2D.Raycast(origin, Vector3.down, 0.2f, groundMask);
-
-        /*
-        if (!_isJumping && _isGrounded)
-        {
-            _isJumping = false;
-            _isFalling = false;
-            _rb.gravityScale = 1;
-        }
-
-        if (_isJumping && _rb.linearVelocityY < 0)
-        {
-            _isJumping = false;
-            _isFalling = true;
-            _rb.gravityScale = 2;
-        }*/
     }
 
     private void FixedUpdate()
@@ -91,6 +141,11 @@ public class PlayerController : MonoBehaviour
             _rb.linearVelocityY = jumpForce;
             _canJump = false;
         }
+    }
+
+    public void OnLifeLost()
+    {
+        _isDead = true;
     }
 
     private void OnDrawGizmos()
